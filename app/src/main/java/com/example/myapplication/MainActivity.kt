37d08db77c950091c79b8f796aef37e3a3afada0
RehanGith <chrehan1007@gmail.com>
2025-03-05 22:06:50 +0500

    Used the Card layout and in which column and image

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,28 +34,21 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun Layout() {
-        var context = LocalContext.current
-        Box(modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center) {
-            Text(text = "Muhammad Rehan", modifier = Modifier
-                .background(Color.Gray)
-                .align(Alignment.TopStart)
-                .fillMaxWidth(), fontSize = 20.sp )
-            Text(text = "Muhammad Rehan", modifier = Modifier
-                .background(Color.Gray)
-                .align(Alignment.CenterStart), fontSize = 20.sp )
-            Text(text = "Muhammad Rehan", modifier = Modifier
-                .background(Color.Gray)
-                .align(Alignment.BottomStart)
-                .fillMaxWidth(), fontSize = 20.sp )
-            Button(onClick = {
-                Toast.makeText(context, "Button Click", Toast.LENGTH_SHORT).show()
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Gray),
+            elevation = CardDefaults.cardElevation(10.dp)
+        ) {
+            Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile),
+                    contentDescription = "my profile pic",
+                    modifier = Modifier.clip(RoundedCornerShape(50.dp))
+                )
+                Text(text = "Muhammad Rehan", textAlign = TextAlign.Center)
             }
-            , modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .background(Color.DarkGray))  {
-                Text(text = "Click me")
-            }
+
         }
     }
 }
