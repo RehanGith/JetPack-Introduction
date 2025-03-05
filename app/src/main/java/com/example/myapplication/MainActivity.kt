afd9879adf78ee7d5ff3e9ca03f073c34c3ce5c6
RehanGith <chrehan1007@gmail.com>
2025-03-05 06:30:49 +0500

    Played with text widget

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,7 +10,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +24,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Text(
                 text = "Hello Rehan!",
+                modifier = Modifier.padding(25.dp),
+                color = Color.Cyan,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 30.sp,
+
             )
         }
     }
