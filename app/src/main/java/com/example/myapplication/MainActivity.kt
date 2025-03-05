95c96540a07ac4db7fa78487a0b23e8d7e88e822
RehanGith <chrehan1007@gmail.com>
2025-03-05 06:49:42 +0500

    played around with text

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,10 +27,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Text(
                 text = "Hello Rehan!",
-                modifier = Modifier.padding(25.dp),
+                modifier = Modifier.fillMaxWidth(1f).background(Color.Gray),
                 color = Color.Cyan,
                 fontFamily = FontFamily.SansSerif,
                 fontSize = 30.sp,
+                textAlign = TextAlign.Center
 
             )
         }
