e0e374e3ebcb3531fa9b2b6c2ac8483aa5fc1499
RehanGith <chrehan1007@gmail.com>
2025-03-06 21:53:09 +0500

    learned about verticalLazyGrid

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,22 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,11 +30,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,17 +47,31 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun Layout() {
-        var textFieldValue by remember {
-            mutableStateOf(TextFieldValue(""))
-        }
-        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-            TextField(value = textFieldValue, onValueChange = {
-                textFieldValue = it
-            }, label = { Text(text = "Name") },
-                placeholder = { Text(text = "Enter your name") }
-                , leadingIcon = { Icon(Icons.Default.Person, contentDescription = "person pic") }
-                , singleLine = true
-            )
+        MyApplicationTheme {
+
+            Scaffold { contentPadding ->
+                LazyVerticalGrid(columns = GridCells.Adaptive(100.dp),
+                    contentPadding = contentPadding,
+                    content =
+                    {
+                        items(100) {
+                            Box(
+                                modifier = Modifier
+                                    .padding(10.dp)
+                                    .background(Color.Green)
+                                    .clip(
+                                        RoundedCornerShape(10.dp)
+                                    )
+                                    .aspectRatio(1f), contentAlignment = Alignment.Center
+                            ) {
+                                Text(text = "Hello $it", fontSize = 20.sp)
+                            }
+                        }
+
+                    }
+                )
+
+            }
         }
     }
 }
